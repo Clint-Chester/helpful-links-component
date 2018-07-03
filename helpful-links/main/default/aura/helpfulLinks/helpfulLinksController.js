344beb56b7bc2aa43578026bc2a7ddd0e527d8bd
Clint Chester <clintchester@clints-mbp.lan>
2018-07-03 22:17:15 +0800

    Updated project structure to Salesforce DX format, and switched the delimiter to a ;

--- helpful-links/main/default/aura/helpfulLinks/helpfulLinksController.js
+++ helpful-links/main/default/aura/helpfulLinks/helpfulLinksController.js
@@ -1,18 +1,16 @@
 ({
 	doInit : function(cmp) {
 		//get the variables to build the link list
-		var linkIcons = cmp.get("v.linkIcons").split(",");
-        var linkTitles = cmp.get("v.linkTitles").split(",");
-        var linkUrls = cmp.get("v.linkUrls").split(",");
-        var linkDescription = cmp.get("v.linkDescription").split(",");
+		var linkIcons = cmp.get("v.linkIcons").split(";");
+        var linkTitles = cmp.get("v.linkTitles").split(";");
+        var linkUrls = cmp.get("v.linkUrls").split(";");
+        var linkDescription = cmp.get("v.linkDescription").split(";");
 
         //check that all the link elements are the same size
         if(linkIcons.length === linkTitles.length && linkIcons.length === linkUrls.length && linkIcons.length === linkDescription.length && linkTitles.length === linkUrls.length && linkTitles.length === linkDescription.length && linkUrls.length === linkDescription.length) {
             //determine correct margin to use for this list
-            var chainLists = cmp.get("v.chainLists");
-            if(chainLists === true) {
-                var linksContainer = cmp.find("linksContainer");
-                $A.util.addClass(linksContainer, "pull-up-margin");
+            if(cmp.get("v.chainLists")) {
+                $A.util.addClass(cmp.find("linksContainer"), "pull-up-margin");
             }
             //now build the list
             var helpfulLinks = cmp.get("v.helpfulLinks");
