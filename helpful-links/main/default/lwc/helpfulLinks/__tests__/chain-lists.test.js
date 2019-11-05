c3b7e277a74132913c399808881c8436cafb934b
Clint Chester <clint.chester@edgewatercricketclub.com>
2019-11-05 23:22:39 +0800

    Component compatible with Lightning Communities (#15)
    * Component compatible with Lightning Communities
    
    * Updating of test library and API version used
    
    * Update of script to install latest test library
    
    * Update of correct config dependency
    
    * Creating elements in the correct scope of the test

--- helpful-links/main/default/lwc/helpfulLinks/__tests__/chain-lists.test.js
+++ helpful-links/main/default/lwc/helpfulLinks/__tests__/chain-lists.test.js
@@ -1,26 +1,25 @@
-// createElement is what we'll use to create our component under test
 import { createElement } from "lwc";
-// helpfulLinks is the component being tested
 import helpfulLinks from "c/helpfulLinks";
-// Mock the navigation mixin
 import { CurrentPageReference } from "lightning/navigation";
-jest.mock("lightning/navigation");
-
-// Construct the common components of the Helpful Links
-let element;
-beforeAll(() => {
-    element = createElement("c-helpful-links", { is: helpfulLinks });
-    element.linkIcons = "doctype:pdf;doctype:flash";
-    element.linkTitles = "Sample Title; Sample Title";
-    element.linkUrlsOrIds = "/;/";
-    element.linkDescriptions = "Sample Description;Sample Description";
-    element.navigationOptions = "URL";
-    CurrentPageReference.mockReturnValue("/");
-});
 
 describe("Helpful Links Component", () => {
+
+    afterEach(() => {
+        // The jsdom instance is shared across test cases in a single file so reset the DOM
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+
     it("is chained to another component", () => {
         // Given
+        let element = createElement("c-helpful-links", { is: helpfulLinks });
+        element.linkIcons = "doctype:pdf;doctype:flash";
+        element.linkTitles = "Sample Title; Sample Title";
+        element.linkUrlsOrIds = "/;/";
+        element.linkDescriptions = "Sample Description;Sample Description";
+        element.navigationOptions = "URL";
+        CurrentPageReference.mockReturnValue("/");
         const expectedClassName = "slds-grid slds-wrap slds-color__background_gray-1 slds-box pull-up-margin";
         // When
         element.chainLists = true;
@@ -31,6 +30,13 @@ describe("Helpful Links Component", () => {
     });
     it("is not chained to another component", () => {
         // Given
+        let element = createElement("c-helpful-links", { is: helpfulLinks });
+        element.linkIcons = "doctype:pdf;doctype:flash";
+        element.linkTitles = "Sample Title; Sample Title";
+        element.linkUrlsOrIds = "/;/";
+        element.linkDescriptions = "Sample Description;Sample Description";
+        element.navigationOptions = "URL";
+        CurrentPageReference.mockReturnValue("/");
         const expectedClassName = "slds-grid slds-wrap slds-color__background_gray-1 slds-box";
         // When
         element.chainLists = false;
