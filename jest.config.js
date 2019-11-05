0033b515fee20b1ddd1a0b9acf09519cb7a0aea1
Clint Chester <chester.clint@gmail.com>
2019-11-05 22:23:00 +0800

    Removed of unused config file

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-const { jestConfig } = require("@salesforce/lwc-jest/config");
-module.exports = {
-    ...jestConfig,
-    moduleNameMapper: {
-        "^lightning/navigation$":
-            "<rootDir>/helpful-links/test/jest-mocks/lightning/navigation"
-    }
-};
